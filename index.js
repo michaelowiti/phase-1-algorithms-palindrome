fb9185910e0e6ae6e87ee85b73dbf36c1546b6fe
mike <mjowiti@gmail.com>
2023-06-29 15:37:58 +0300

    completed

--- index.js
+++ index.js
@@ -1,5 +1,75 @@
+// let word=""
+// function reversedString(word){
+// const reverseword=word.split("").reverse().join("")
+
+// return  reverseword
+// }
+
+
+// function isPalindrome(word) {
+//   // Write your algorithm here
+//   if(word===reverseword){
+//     console.log("true")
+//   return true;
+//   }
+    
+//     else{
+//       console.log("false")
+//       return false
+
+//     }
+//   }
+
+
+// /* 
+//   Add your pseudocode here
+//   first of all the input is a string
+//   if it is a palindrome return true
+//   if not return false
+
+// */
+
+// /*
+//   Add written explanation of your solution here
+
+//   if the input string is a palindrome then return true else u return false.We might as well us an if sataement
+// */
+// // You can run `node index.js` to view these console logs
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
+
+// module.exports = isPalindrome;
+
+
+
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
@@ -12,9 +82,28 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
